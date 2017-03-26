70445d19437abd8d5461177a53efb9be5eeb7622
zaiblitz <merwindale.domingo@gmail.com>
2017-03-26 16:19:33 -0700

    Load static files

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mysql = require('mysql');
+
+var path = require('path');
+
+
+// Load css js files
+app.use("/bower_components",express.static("bower_components"));
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
-  });
+	res.sendfile('index.html');
+});
 
-  http.listen(3000, function(){
-    console.log('listening on *:3000');
-    });
+
+var totalUsers = 0;
+
+//Whenever someone connects this gets executed
+io.on('connection', function(socket){
+	totalUsers++;
+
+
+ 	// Whenever someone disconnects this piece of code executed
+	socket.on('disconnect', function () {
+		totalUsers--;
+	});
+});
+
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
